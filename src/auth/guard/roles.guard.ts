bfbb96135fbcd259ec889f3c0c8c02a07ebe12c1
Mimachh <karl.mullr@gmail.com>
2023-12-11 16:28:35 +0100

    pb pour la verification des rôles

--- src/auth/guard/roles.guard.ts
+++ src/auth/guard/roles.guard.ts
@@ -1,7 +1,36 @@
-import { AuthGuard } from '@nestjs/passport';
+// import { AuthGuard } from '@nestjs/passport';
 
-export class rolesGuard extends AuthGuard('jwt') {
-  constructor() {
-    super();
+// export class rolesGuard extends AuthGuard('jwt') {
+//   constructor() {
+//     super();
+//   }
+// }
+
+// import { AuthGuard } from '@nestjs/passport';
+
+// export class rolesGuard extends AuthGuard('jwt') {
+//   constructor() {
+//     super();
+//   }
+// }
+
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common"
+
+
+@Injectable()
+export class VerifyRoles implements CanActivate {
+  
+  canActivate(ctx: ExecutionContext) {
+
+    const request = ctx.switchToHttp().getRequest()
+    
+    if (request?.user) {
+      const { id } = request.user;
+      console.log(id);
+    }
+    const { user } = ctx.switchToHttp().getRequest();
+    // const user = request.user;
+    console.log({request})
+    return true;
   }
 }
