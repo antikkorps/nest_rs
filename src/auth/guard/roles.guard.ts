ff00d9c62dd8a890d128806371670931253d0ccf
Mimachh <karl.mullr@gmail.com>
2023-12-12 16:57:33 +0100

    suppression du trop plein de role.guard

--- src/auth/guard/roles.guard.ts
+++ src/auth/guard/roles.guard.ts
@@ -1,43 +1,4 @@
 import { AuthGuard } from '@nestjs/passport';
-
-// export class rolesGuard extends AuthGuard('jwt') {
-//   constructor() {
-//     super();
-//   }
-// }
-
-// import { AuthGuard } from '@nestjs/passport';
-
-// export class rolesGuard extends AuthGuard('jwt') {
-//   constructor() {
-//     super();
-//   }
-// }
-
-// import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common"
-
-
-// @Injectable()
-// export class VerifyRoles extends AuthGuard('jwt') implements CanActivate {
-  
-//   canActivate(ctx: ExecutionContext) {
-
-//     const request= ctx.switchToHttp().getRequest();
-    
-//     if (request?.user) {
-//       const user = request.user;
-//       // console.log({user})
-
-//       console.log(user)
-//       // const user = await this.prisma.user.findUnique({ where: { id: userId } });
-      
-//     }
-
-   
-//     return true;
-//   }
-// }
-
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 
 @Injectable()
